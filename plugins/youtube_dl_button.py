aef9c9f1f5d6199169b032876525f68a84071ef1
appusejithin33 <68550804+appusejithin33@users.noreply.github.com>
2021-02-07 16:58:58 +0400

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -276,7 +276,7 @@ async def youtube_dl_call_back(bot, update):
                     chat_id=update.message.chat.id,
                     document=download_directory,
                     thumb=thumb_image_path,
-                    caption=f"<b>{file_name}</b>",
+                    caption=description,
                     parse_mode="HTML",
                     # reply_markup=reply_markup,
                     reply_to_message_id=update.message.reply_to_message.message_id,
@@ -306,7 +306,7 @@ async def youtube_dl_call_back(bot, update):
                 await bot.send_video(
                     chat_id=update.message.chat.id,
                     video=download_directory,
-                    caption=f"<b>{file_name}</b>",
+                    caption=description,
                     parse_mode="HTML",
                     duration=duration,
                     width=width,
